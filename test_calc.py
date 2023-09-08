dd089fa5783dcd1e1cf57ae99ba65aa2766922b6
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-09-08 15:24:50 -0400

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,11 +1,11 @@
 from calc import mul , sub , add
 
 def test_mul():
-    assert calc.mul(1, 2) == 2
+    assert mul(1, 2) == 2
 def test_sub():
-    assert calc.sub(1, 2) == -1
+    assert sub(1, 2) == -1
 def test_add():
-    assert calc.add(1,2) == 3
+    assert add(1,2) == 3
 
 test_mul()
 test_sub()
